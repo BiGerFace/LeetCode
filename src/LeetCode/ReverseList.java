e29f5d825ff77ed8f80fcab932988b815e06a3b8
zhangm <zhangm@c-goshine.com>
2018-11-01 14:29:47 +0800

    git测试

---
+++ src/LeetCode/ReverseList.java
@@ -0,0 +1,28 @@
+package LeetCode;
+
+/**
+ *  单链表逆置
+ *  Reverse a singly linked list
+ *
+ *
+ */
+public class ReverseList {
+
+  /*  public ListNode reverseList(ListNode head) {
+        // 0或1个节点直接返回
+        if(head == null){
+            return null;
+        }
+        if(head.next == null){
+            return head;
+        }
+        ListNode h = head;
+        while(head.next!=null){
+            ListNode p =head.next;
+            head.next = p.next;
+            p.next=h;
+            h=p;
+        }
+        return h;
+    }*/
+}
